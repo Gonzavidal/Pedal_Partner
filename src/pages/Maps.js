62d4d44e35f04afe2b9408a2e6da6153874c6b8f
Gonzavidal <gvidal1992@gmail.com>
2023-04-26 20:21:50 +0000

    Maps y marcadores

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '400px',
   height: '400px'
 };
 
-const center = { 
-  lat: -33.742145,  lng: -70.735680
+const center = {
+  lat: -33.742145, lng: -70.735680
 };
+
+const position = {
+  lat: -33.74200819688481,
+  lng: -70.73546492713628
+}
 
 function Maps() {
   const { isLoaded } = useJsApiLoader({
@@ -21,7 +26,8 @@ function Maps() {
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
+    
+    /*map.fitBounds(bounds);*/
 
     setMap(map)
   }, [])
@@ -31,16 +37,18 @@ function Maps() {
   }, [])
 
   return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={5}
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={20} // 
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      <Marker
         onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
+        position={position}
+      />
+    </GoogleMap>
   ) : <></>
 }
 
